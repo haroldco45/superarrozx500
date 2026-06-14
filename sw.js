35bb9b18f5988404fab2e96aee5c7daf5faef99d
haroldco45 <68300211+haroldco45@users.noreply.github.com>
2026-06-14 05:41:35 -0500

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'superarroz-v1';
+const ASSETS = [
+  './index.html',
+  './app.js',
+  './manifest.json',
+  './icons/placeholder.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(()=>{})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(()=>cached))
+  );
+});
